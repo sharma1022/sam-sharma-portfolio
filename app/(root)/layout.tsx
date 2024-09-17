4695769a9f7ac3ebc36c4c12ada5e7071055fb26
Sam Sharma <samsharma.dev@gmail.com>
2024-09-16 21:16:31 -0700

    add routing

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,15 @@
+
+import Image from "next/image";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  
+  return (
+    <main>
+        {children}
+    </main>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/utils/cn";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -19,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${montserrat.className}`}
+        className={cn("min-h-screen",montserrat.className)}
       >
         {children}
       </body>
