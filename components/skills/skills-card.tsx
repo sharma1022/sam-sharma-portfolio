2f829db75b3b71daa8cdfff1830bd273ec9f7dbc
Sam Sharma <samsharma.dev@gmail.com>
2024-09-19 12:44:25 -0700

    add a placeholder component for skills card

---
+++ components/skills/skills-card.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SkillsCard = () => {
+  return (
+    <div>SkillsCard</div>
+  )
+}
+
+export default SkillsCard
--- components/skills/skills-section.tsx
+++ components/skills/skills-section.tsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const SkillSection = () => {
   return (
-    <div>SkillSection</div>
+    <section className="overflow-hidden px-6 py-32 sm:px-14 md:px-20">
+      <div className="relative mx-auto max-w-7xl">
+        <h2 className="text-xl font-semibold text-accent sm:text-4xl">
+          Skills
+        </h2>
+        </div>
+    </section>
   )
 }
 
